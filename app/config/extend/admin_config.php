6df58ef90974ab85fb0a7ea4f8515dac24540001
nguyenanhung <hungna.dev@gmail.com>
2024-09-08 15:15:48 +0700

    Update config admin_config_basic_auth

--- app/config/extend/admin_config.php
+++ app/config/extend/admin_config.php
@@ -8,6 +8,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
  * Time: 11:39
  */
 $config['admin_config_basic_auth'] = [
+    'serviceId' => 'CodeIgniter skeleton',
+    'framework' => 'CodeIgniter v' . CI_VERSION,
     'username' => 'hungna',
     'password' => 'xxx'
 ];
